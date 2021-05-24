f0b16f14de4bf1374720f4d80ba2932e35f224e4
Ellis Brown <ellis_brown@apple.com>
2021-05-24 14:04:25 -0400

    Fixed some styling issues with /Jupyter-everything/scripts/plot_data.py   TODO: Presentation of data into squares similar to GC-EASY analysis.

--- Jupyter-everything/scripts/TEMP_driver.py
+++ Jupyter-everything/scripts/TEMP_driver.py
@@ -4,7 +4,7 @@ import plot_data as pltd
 def main():
     pl.setLogPath("../../datasets/gc.log")
     df = pl.getPauses(False)
-    #pltd.plot_pauses(df)
-    print(df)
+    pltd.plot_pauses(df)
+   
 
 main()
--- Jupyter-everything/scripts/plot_data.py
+++ Jupyter-everything/scripts/plot_data.py
@@ -7,30 +7,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def plot_pauses(df):
+    # Obtain X Y list information from the dataframe.
     x_values = list(df.iloc[:,3])
     x = []
     for entry in x_values:
         x.append(entry[:-1])
-    y_values = list(df.iloc[:,0])
-    ## Only plot the first 5, due to label confusion. TODO.
-    max_items = 10 #len(y_values) for max
-    plt.bar(x_values[:max_items], heights= y_values[:max_items])
+    x_values = list(map(float, x))
+    y_values = map(float, list(df.iloc[:,0]))
+    del x #not necesarily needed
 
-    plt.ylabel("Miliseconds wait");
-
+    plt.bar(x = x_values, height= y_values)
+    plt.ylabel("Pause duration (miliseconds)");
+    plt.xlabel("Time from program start (seconds)")
+    plt.title("Pauses for Young Generation GC")
     ## Find interesting trends within the data.
-   
-    find_trends(df)
+    __find_trends(df)
 
 # Finds trends in dataframe. 
 # Column 1 must be pause time, (1 indexed)
 # Column 4 must be time since start of program.
- def find_trends(df):
-        wait_times = list(df.iloc[:,0])
-        max_wait = max(wait_times, key = lambda i : float(i))
-        print("Max wait: " + str(max_wait) + " ms")
-        
-        total_wait   = round(sum(float(i) for i in wait_times), 4)
-        average_wait = round(total_wait / len(wait_times), 4)
-        print("Total wait: " + str(total_wait) + " ms")
-        print("Average wait: " + str(average_wait) + " ms")
+def __find_trends(df):
+    wait_times = list(df.iloc[:,0])
+    max_wait = max(wait_times, key = lambda i : float(i))
+    print("Max wait: " + str(max_wait) + " ms")
+    
+    total_wait   = round(sum(float(i) for i in wait_times), 4)
+    average_wait = round(total_wait / len(wait_times), 4)
+    print("Total wait: " + str(total_wait) + " ms")
+    print("Average wait: " + str(average_wait) + " ms")
