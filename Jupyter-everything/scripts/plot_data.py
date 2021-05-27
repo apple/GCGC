9ca066718eef3e349b319ec5d2da4fcbc597f68b
Ellis Brown <ellis_brown@apple.com>
2021-05-27 10:27:19 -0400

    Added documentation for calculate freemem

--- Jupyter-everything/scripts/plot_data.py
+++ Jupyter-everything/scripts/plot_data.py
@@ -86,14 +86,33 @@ def plot_heap_allocation_breakdown(counts):
     for idx in range(len(counts[0])):
         plt.plot(x, np.array(list(row[idx] for row in counts)), color = colors[idx], label = region_names[idx])
     plt.legend()
-    plt.show() # commented out during testing.
+    plt.show() 
 
-# Purpose : Plot the heap breakdown throughout program
-#
-# Counts is a dictionary. Keys = names
-# Values = list of tuples
-#                  (before, after)
+
+## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                   __plot_HA_schema0                                          #
+#   Purpose:                                                                   #
+#       Plot the heap breakdown throughout program using memory collected      #
+#       following a log schema0. Display 3 plots                               #
+#       -> Memory regions during runtime (without free memory)                 #
+#       -> Free heap memory regions                                            #
+#       -> Memory regions + free regions during runtime                        #
+#                                                                              #
+#   Parameters:                                                                #
+#       dd : list, 2 items                                                     #
+#       dd[0] : dictionary containing                                          #
+#               keys:   names of regions during runtime                        #
+#              values:  list of before/after tuple pairs of size when gc runs  #
+#       dd[1] : Initial number of free regions before runtime.                 #
+#                                                                              #    
+#   Return: None                                                               #
+#                                                                              #
+#   Note: generates MatPlotLib plot                                            #
+## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 def __plot_HA_schema0(dd):
+    if (not dd) or (len(dd) < 2) or (not dd[0]) or (not dd[1]):
+        return
+
     data_dictionary = dd[0]
     free_memory = __calculate_freemem(data_dictionary, dd[1])
     x = np.array(list(range(len(data_dictionary["Eden"]) * 2))) 
@@ -112,11 +131,11 @@ def __plot_HA_schema0(dd):
         temp_plot_for_testing_please_remove_after = plt.plot(np.array(x), np.array(pairs), color = colors[color_index], label = str(key))
         color_index += 1
     plt.legend()
-    plt.show() # commented out during testing.
+    plt.show() 
     plt.figure(2)
     plt.plot(x, np.array(free_memory), color = "red", label = "Free Memory")
     plt.legend()
-    plt.show() # commented out during testing.
+    plt.show() 
     plt.figure(3)
     for key in data_dictionary.keys():
         pairs = []
@@ -127,36 +146,63 @@ def __plot_HA_schema0(dd):
         color_index += 1
     plt.plot(x, np.array(free_memory), color = "red", label = "Free Memory")
     plt.legend()
-    plt.show() # commented out during testing.
+    plt.show() 
 
 
-# Purpose: Because "free" memory is not explicitly captured, must be calculated
+## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                   __calculate_freemem                                        #
+#   Purpose:                                                                   #
+#       Creates a list of the free memory chunks throughout a                  #
+#       program's runtime based on the starting values                         #
+#   Parameters:                                                                #
+#       data_dictionary : dictionary containing the free regions               #
+#                           (keys):   name of each type of data region         #
+#                         (values):   tuple containing before/after count for  #
+#                                     the region.                              #
+#       inital_free     : integer value with the inital number of free regions #
+#                                                                              #
+#   Return: list with the # of free regions sequentially. Memory recorded      #
+#           directly before and after each garbage collection pause.           #
+## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 def __calculate_freemem(data_dictionary, inital_free):
+    if not data_dictionary:
+        return []
+
     free_mem = []
     for idx in range(len(data_dictionary["Eden"])):
+        
+        # Calculate the free memory before the GC runs
         temp_val = 0
         for key in data_dictionary.keys():
+                                        # access tuple [0] from list
+                                        # of tuples associated with key
             temp_val += int(data_dictionary[key][idx][0])
         free_mem.append(int(inital_free - temp_val))
+        
+        # Calculate the free memory after the GC runs
         temp_val = 0
         for key in data_dictionary.keys():
             temp_val += int(data_dictionary[key][idx][1])
         free_mem.append(int(inital_free - temp_val))
+
     return free_mem
         
+######################################################################
+# Following block is currently unused.
+# Rational: It returns the same data as tableMetadata(), just less 
+# # Purpose: Creates a graphical table to represent the initial heap state
+# # Parameters: inital_heap_state (hs) : dict
+# # Return: None
+# def tableInitialHeapState(hs):
+#     # create table from data
+#     if hs:
+#         table_rows = [[key, hs[key]] for key in hs.keys()]
+#         for item in table_rows:
+#             print(item[0] + " \t| " + item[1])
+#     else:
+#         print("No found heap state information. Empty dict")
+######################################################################
 
-
-# Purpose: Creates a graphical table to represent the initial heap state
-# Parameters: inital_heap_state (hs) : dict
-# Return: None
-def tableInitialHeapState(hs):
-    # create table from data
-    if hs:
-        table_rows = [[key, hs[key]] for key in hs.keys()]
-        for item in table_rows:
-            print(item[0] + " \t| " + item[1])
-    else:
-        print("No found heap state information. Empty dict")
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
