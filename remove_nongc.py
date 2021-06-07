0feb6b367c161af74e508816899cacf73f074609
Ellis Brown <ellis_brown@apple.com>
2021-06-07 17:29:14 -0400

    Removed no-longer-used file ./remove_nongc.py

--- remove_nongc.py
+++
@@ -1,7 +0,0 @@
-with open("/Users/ellisbrown/Desktop/JFR_folder/temp_out", "r") as file:
-    with open("./datasets/linux_ubuntu.log", "w") as out:
-        data = file.readlines()
-        for line in data:
-            if line[0] == "[":
-                out.write(line)
-                
