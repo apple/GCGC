60dd8505e8986fd9d21870b8acc1e369b0ba29c4
Ellis Brown <ellis_brown@apple.com>
2021-07-27 18:45:10 -0400

    Group into functions, improve documentation for filter and group

--- src/filter_and_group.py
+++ src/filter_and_group.py
@@ -1,4 +1,7 @@
 #       filter_and_group.py
+#   
+#   Given a set of filters, and groupings, take data from pandas dataframes, 
+#   and return a modified subset of the data that follows the passed filters/groups.
 #
 # Ellis Brown
 # 7-12-2021
@@ -20,77 +23,32 @@ def filter_and_group(
     colors=None, # a list colors. If none are provided, determinsitic colors returned for dataset
     column_timing = None # Overrides the timing column to collect, if provided. All values in the column must be ints/floats
 ):
+    # Apply the filters, if any
     if filter_by:
         datasets = apply_filter(datasets, filter_by)
+    # Create the labels if non provided
     if not labels:
         labels = [str(num + 1) for num in range(len(datasets))]
-
-    # After verifying/setting paramets, loop through all datasets.
-    timestamp_groups = [] # For time of event 
-    datapoint_groups = [] # For data in 'column'
-    group_labels = []
-    log_number_list = [] # used to specify which dataset in original list
     if not column_timing:
         column_timing = "TimeFromStart_seconds"
-    
-    if group_by: # We need to create groups within each dataset
-        
-        for idx, df in enumerate(datasets):
-            if not df.empty:
-                if group_by not in df:
-                    print("Warning: group_by group " + str(group_by) + " column not in dataset with columns " + str(df.columns))
-                elif column not in df:
-                    print("Warning: column \"" + str(column) + "\" not in dataset with columns " + str(df.columns))
-                else:
-                    if column_timing in df:
-                        groups = {}
-                        if column_timing == "DateTime":
-                            timing = matplotlib.dates.date2num(df[column_timing])
-                        else:
-                            timing = df[column_timing]
-                        for group, time, datapoint in zip(df[group_by], timing, df[column]):
-                            if group:
-                                if group not in groups:
-                                    # Create a new group for each unique item
-                                    groups[group] = [[], [], str(labels[idx]) + ": " + str(group)]
-                                # add the datapoints and time, based on the grouping
-                                
-                                groups[group][0].append(time)
-                                groups[group][1].append(datapoint)
+    # Group into lists of X/Y associated data with labels.
+    timestamp_groups = [] # For time of event 
+    datapoint_groups = [] # For data in 'column'
+    group_labels = []     # Label to descrbe the group.
+    if group_by:
+        print("Case GROUP BY")
+        timestamp_groups, datapoint_groups, group_labels = arrange_into_groups(datasets, group_by, column, column_timing, labels)
+    else:
+        timestamp_groups, datapoint_groups, group_labels = arrange_no_groups(datasets, column, column_timing, labels)
 
-                        # Sort keys so groups print in the same order between files
-                        keys = list(groups.keys())
-                        keys.sort()
-
-                        for key in keys:
-                            timestamp_groups.append(groups[key][0])
-                            datapoint_groups.append(groups[key][1])
-                            group_labels.append(groups[key][2])
-                            log_number_list.append(idx)
-                    else:
-                        print("Warning: column_timing \"" + str(column_timing) + "\" not in dataset with columns " + str(df.columns))
-                
-    else: # no groups are required, just use the entire filtered dataset as a group 
-        
-        for idx, df in enumerate(datasets):
-            if not df.empty:
-                if column_timing in df:
-                    if column in df:
-                        if column_timing == "DateTime":
-                            timestamp_groups.append(matplotlib.dates.date2num(df[column_timing]))
-                        else:
-                            timestamp_groups.append(df[column_timing])
-                        datapoint_groups.append(df[column])
-                        group_labels.append(labels[idx])
-                        log_number_list.append(idx)
-                        
-    # Determine the colors from the dataset. If colors were passed, use those instead.  
+    # Add the colors.
     if not colors:
-        colors, alphas = get_colors_and_alphas(log_number_list)
+        colors, alphas = get_colors_and_alphas(len(group_labels))
     else:
         alphas = [1 for i in range(len(colors))]
 
     return timestamp_groups, datapoint_groups, group_labels, colors, alphas
+
 
 import pandas as pd
 #       apply_filter
@@ -107,13 +65,21 @@ def apply_filter(datasets, filter_by=None):
 
         # the reason to use index is to update the actual value
         for i in range(len(dfs)): 
+            
+            # Apply functions return a boolean. Only retain rows that evaluate to True
             dfs[i] = dfs[i][dfs[i].apply(filter_by, axis=1)]
+    
     else:
+        # Return the same data if no filters needed
         dfs = datasets
     return dfs
 
-
-def get_colors_and_alphas(log_no_list):
+#       get_colors_and_alphas
+#
+#   Given then number of colors, returns that many colors from a preset
+#   sequence of repeating colors, begging at the start of the sequence
+#
+def get_colors_and_alphas(number_of_colors):
     preset_colors = [(230/255, 25/255, 75/255),
                     (60/255, 180/255, 75/255),
                     (215/255, 215/255, 25/255),
@@ -137,9 +103,78 @@ def get_colors_and_alphas(log_no_list):
                     (0, 0, 0)] # https://sashamaps.net/docs/resources/20-colors/
     colors = []
     alphas = []
-    while len(log_no_list) > len(preset_colors):
+    while number_of_colors > len(preset_colors): # In the case where we need MANY colors, copy the cycle and add more.
         preset_colors = preset_colors + preset_colors
-    for idx in range(len(log_no_list)):
+
+    for idx in range(number_of_colors):
         colors.append(preset_colors[idx])
         alphas.append(1)
     return colors, alphas
+
+#       arrange_into_groups
+#
+#   Given a grouping pattern, and set of filtered datasets, creates a list of
+#   X and Y datalists for each group found in the passed dataset.
+#
+def arrange_into_groups(datasets, group_by, column, column_timing, labels):
+        timestamp_groups = []
+        datapoint_groups = []
+        group_labels = []
+        for idx, df in enumerate(datasets): # Loop through all provided log datasets
+            if not df.empty:
+                if group_by not in df:
+                    print("Warning: group_by group " + str(group_by) + " column not in dataset with columns " + str(df.columns))
+                elif column not in df:
+                    print("Warning: column \"" + str(column) + "\" not in dataset with columns " + str(df.columns))
+                elif column_timing not in df:
+                    print("Warning: column_timing \"" + str(column_timing) + "\" not in dataset with columns " + str(df.columns))
+                else: 
+                    # A non-empty df contains both X and Y columns.                    
+                    
+                    groups = {} # Create a dictionary to hold unique groups
+                    if column_timing == "DateTime":
+                        timing = matplotlib.dates.date2num(df[column_timing])
+                    else:
+                        timing = df[column_timing]
+                    for group, time, datapoint in zip(df[group_by], timing, df[column]):
+                        if group:
+                            if group not in groups:
+                                # Create a new group for each unique item
+                                groups[group] = [[], [], str(labels[idx]) + ": " + str(group)]
+                            
+                            # add the datapoints and time, based on the grouping
+                            groups[group][0].append(time)
+                            groups[group][1].append(datapoint)
+
+                    # Sort keys so groups print in the same order between files
+                    keys = list(groups.keys())
+                    keys.sort()
+
+                    for key in keys:
+                        timestamp_groups.append(groups[key][0])
+                        datapoint_groups.append(groups[key][1])
+                        group_labels.append(groups[key][2])
+                        
+                    
+        return timestamp_groups, datapoint_groups, group_labels
+    
+#       arrange_no_groups
+#
+#   Given no grouping pattern, take data from datasets, place them
+#   into of X and Y datasets.
+#
+def arrange_no_groups(datasets, column, column_timing, labels):
+    timestamp_groups = []
+    datapoint_groups = []
+    group_labels = [] # Included in case no data is extracted from a df
+    for idx, df in enumerate(datasets):
+        # Make sure both the columns are present, and rows are present
+        if not df.empty and column_timing in df and column in df:
+            if column_timing == "DateTime":
+                timestamp_groups.append(matplotlib.dates.date2num(df[column_timing]))
+            else:
+                timestamp_groups.append(df[column_timing])
+            datapoint_groups.append(df[column])
+            group_labels.append(labels[idx])
+                
+    return timestamp_groups, datapoint_groups, group_labels
