9338511f80edd0e57f67207ae5923e2b0abcc456
Ellis Brown <ellis_brown@apple.com>
2021-08-06 15:59:07 -0400

    Added special color to frequency = 0.

--- src/graphing/heatmap.py
+++ src/graphing/heatmap.py
@@ -27,11 +27,11 @@ def plot_heatmap(heatmap, dimensions, labels=True):
     height = dimensions[1]  # y_bucket_count
     max_pause_ms = height * dimensions[3]
     max_time_ms = width * dimensions[2]
-    min_pause_ms = 0
+    min_time_ms = dimensions[4]
     multipler = max_time_ms / width  # multipler is the size of a bucket for time direction
-
     # x labels are the time labels
-    time_labels = [num * multipler for num in range(1, width + 1)]  # TODO : UPDATE TO BE FASTER
+    print(min_time_ms)
+    time_labels = [num * multipler + min_time_ms for num in range(1, width + 1)]  # TODO : UPDATE TO BE FASTER
 
     time_labels_temp = []
     for i in range(len(time_labels)):
@@ -44,9 +44,9 @@ def plot_heatmap(heatmap, dimensions, labels=True):
     time_labels = time_labels_temp
 
     # size of the buckets for ms pause
-    multipler = (max_pause_ms - min_pause_ms) / height
+    multipler = (max_pause_ms) / height
     # y labels are ms pause time labels
-    latency_labels = [round((num * multipler) + min_pause_ms, 2) for num in reversed(range(1, height + 1))]
+    latency_labels = [round((num * multipler), 2) for num in reversed(range(1, height + 1))]
     latency_labels = [str(label) + " ms" for label in latency_labels]
 
     ## Create a figure, and add data to heatmap. Plot then show heatmap.
@@ -54,96 +54,13 @@ def plot_heatmap(heatmap, dimensions, labels=True):
     ax.set_title("Latency during runtime.")
     # cmap is the color scheme. To change the heatmap color scheme, use a color scheme from here:
     # https://matplotlib.org/stable/tutorials/colors/colormaps.html
-    im = heatmap_make(heatmap, latency_labels, time_labels, ax=ax, cmap="cubehelix_r", cbarlabel="Frequency")
+    im = heatmap_make(heatmap, latency_labels, time_labels, ax=ax, cmap="cubehelix_r", cbarlabel="Frequency", fig = fig)
+
     if labels:
         __annotate_heatmap(im, valfmt="{x}")
     fig.tight_layout()
     return ax
-    ## end new
-    """
-    fig, ax = plt.subplots()
-    im = ax.imshow(np.array(heatmap))
 
-    # EVERYTHING ELSE BELOW IS TAKEN FROM MATPLOTLIB DOCUMENTATION
-    # SEE HERE: https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html 
-
-    # We want to show all ticks...
-    ax.set_xticks(np.arange(len(time_labels))) # x axis
-    ax.set_yticks(np.arange(len(latency_labels))) # y axis
-    # ... and label them with the respective list entries
-    ax.set_xticklabels(time_labels)
-    ax.set_yticklabels(latency_labels)
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
-    # Loop over data dimensions and create text annotations.
-    for i in range(len(latency_labels)):
-        for j in range(len(time_labels)):
-            text = ax.text(j, i, heatmap[i, j],
-                           ha="center", va="center", color="w")
-
-    
-
-    fig.tight_layout()
-    plt.show()
-    """
-
-
-################################################
-# Gathers data to properly plot a heatmap.
-# Parameters:
-#   table: a pandas dataframe.  Notable columns:
-#       TimeFromStart_seconds - time in seconds from program start. float
-#       PauseDuration_miliseconds - time in miliseconds for a pause. float
-#   num_b : number of buckets to sort time into.
-#       Note: More buckets = more percise heatmap, labels less clear.
-################################################
-def __get_heatmap(table, width=20, height=20):
-    if table.empty:
-        return
-    # access the two columns from the table with our time/pause info
-    timestamps_seconds = table["TimeFromStart_seconds"]
-    pauses_ms = table["PauseDuration_miliseconds"]
-
-    # create buckets to store the time information.
-    # first, compress into num_b buckets along the time X-axis.
-    x_b = [[] for i in range(width)]
-    max_time_ms = list(timestamps_seconds)[-1]
-    bucket_time_duration = max_time_ms / width
-
-    # populate buckets along the x axis.
-    for pause, time in zip(pauses_ms, timestamps_seconds):
-        bucket_no = int(time / bucket_time_duration)
-        if bucket_no >= width:
-            bucket_no = width - 1
-        x_b[bucket_no].append(pause)
-
-    max_pause_ms = max(pauses_ms)
-    min_pause_ms = min(pauses_ms)
-
-    # calculate the size of the buckets representing a pause
-    bucket_pause_duration = (max_pause_ms - min_pause_ms) / height
-
-    # create heatmap, which will be a 2d-array
-    heatmap = []
-
-    # go through each time interval, and sort the pauses there into frequency lists
-    for bucket in x_b:
-        yb = [0 for i in range(height)]  # construct a 0 frequency list
-        for time in bucket:
-            # determine which ms pause bucket
-            y_bucket_no = int((time - min_pause_ms) / bucket_pause_duration)
-            if y_bucket_no >= height:
-                y_bucket_no = height - 1
-
-            # increase the frequency of that pause in this time interval
-            yb[y_bucket_no] += 1
-
-        # Add the data to the 2d array
-        heatmap.append(yb)
-    heatmap = np.rot90(heatmap)  # fix orientation
-    return np.array(heatmap), min_pause_ms, max_pause_ms, max_time_ms  # all data needed to plot a heatmap.
 
 
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -151,7 +68,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 ## The following code block was taken directly from matplotlib's documentation
 ## seen here:
 # https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
-def heatmap_make(data, row_labels, col_labels, ax=None, cbar_kw={}, cbarlabel="", **kwargs):
+def heatmap_make(data, row_labels, col_labels, ax=None, cbar_kw={}, cbarlabel="", fig = None, **kwargs):
     """
     Create a heatmap from a numpy array and two lists of labels.
 
@@ -178,20 +95,21 @@ def heatmap_make(data, row_labels, col_labels, ax=None, cbar_kw={}, cbarlabel=""
         ax = plt.gca()
 
     # Plot the heatmap
-    im = ax.imshow(data, **kwargs)
+    threshold = 0.5
+    im = ax.imshow(data,  vmin=threshold, **kwargs)
 
     # Create colorbar
     #
-
     divider = make_axes_locatable(ax)
+    
     cax = divider.append_axes("right", size="5%", pad=0.1)
-    plt.colorbar(im, cax=cax)
+    cbar = plt.colorbar(im, cax=cax, extend='min')
+    cbar.cmap.set_under('grey')
 
     # cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
     cax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
 
     # We want to show all ticks...
-
     ax.set_xticks(np.arange(data.shape[1]))
     ax.set_yticks(np.arange(data.shape[0]))
     # ... and label them with the respective list entries.
@@ -305,21 +223,27 @@ def get_heatmap_data(timestamp_groups, datapoint_groups, labels, dimensions):
             print("Warning: All dimensions must be greater than zero.")
             return np.array([]), []
 
+    # Determine minimum time
+    min_time_duration = __get_minimum_time(timestamp_groups)
+    print(min_time_duration)
+    ## Scale minimum time to be a multiple of the time interval, floored.
+    min_time_duration = int(min_time_duration - (min_time_duration % x_bucket_duration))
+    print(min_time_duration)
+    print("Min time", min_time_duration)
+    
     heatmap_list = []
     for times_seconds, pauses_ms, label in zip(timestamp_groups, datapoint_groups, labels):
         if not list(times_seconds) or not list(pauses_ms):
             continue # Skip this loop iteration
-
 
         # create buckets to store the time information.
         # first, compress into num_b buckets along the time X-axis.
         x_b = [[] for i in range(x_bucket_count)]
 
         out_of_range_time = 0
-        # populate buckets along the x axis.
+        # populate buckets with latency data along the x axis.
         for pause, time in zip(pauses_ms, times_seconds):
-            bucket_no = int(time / x_bucket_duration)
-            
+            bucket_no = int((time - min_time_duration) / x_bucket_duration)
            
             if bucket_no == x_bucket_count:
                 bucket_no = x_bucket_count - 1
@@ -345,9 +269,7 @@ def get_heatmap_data(timestamp_groups, datapoint_groups, labels, dimensions):
             for time in bucket:
                 # determine which ms pause bucket
                 if time:
-                    y_bucket_no = int(time / y_bucket_duration)
-                
-                    
+                    y_bucket_no = int(time / y_bucket_duration)                    
                     
                     if y_bucket_no < y_bucket_count:
                         # increase the frequency of that pause in this time interval
@@ -357,8 +279,6 @@ def get_heatmap_data(timestamp_groups, datapoint_groups, labels, dimensions):
                         yb[y_bucket_no] += 1
                     else:
                         out_of_range_latency += 1
-
-
 
             # Add the data to the 2d array
             heatmap.append(yb)
@@ -372,6 +292,7 @@ def get_heatmap_data(timestamp_groups, datapoint_groups, labels, dimensions):
         heatmap_list.append(heatmap)
         if not heatmap_list:
             print("Warning! No heatmap list analyze_logs_dev 384")
+    dimensions.append(min_time_duration)
     return heatmap_list, dimensions
 
 
@@ -481,7 +402,6 @@ def plot_heatmap_logarithmic(heatmap, dimensions, labels=True):
     height = dimensions[1]  # y_bucket_count
     max_pause_ms = height * dimensions[3]
     max_time_ms = width * dimensions[2]
-    min_pause_ms = 0
     multipler = max_time_ms / width  # multipler is the size of a bucket for time direction
 
     # x labels are the time labels
@@ -557,3 +477,14 @@ def binary_search(arr, x):
             high = mid - 1
     # If we reach here, then the element was not present
     return -1
+
+def __get_minimum_time(timestamp_lists):
+    if not timestamp_lists:
+        print("Fatal error: No timestamps collected. Abort")
+        return None
+    
+    min_time = timestamp_lists[0].iloc[0]
+    for timestamp_list in timestamp_lists:
+        min_time = min(timestamp_list.min(), min_time)
+    print("Minimum time recorded in __get_minimum_time", min_time)
+    return min_time 
