231b4e41c96675fda83be7b76ffb54ad39da0405
Ellis Brown <ellis_brown@apple.com>
2021-08-09 12:14:19 -0400

    Remove unused functions. Add zero_time_range.

--- src/temporary_read_log_file.py
+++ src/temporary_read_log_file.py
@@ -57,141 +57,6 @@ def get_gc_event_tables(files, time_range_seconds, ignore_crashes = False):
 
 
 ## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                               get_parsed_data_from_file
-#
-# Purpose:
-#   Given a filepath to a log file, create a pandas dataframe holding all information
-#   for that log file. Each row is a different event.
-#
-# Parameters:
-#   logfile -> a string with the path to the log file to read.
-#
-# Return:
-#   a pandas dataframe, where rows are each an individual event. Columns are labeled, and
-#   collect information on each event, such as when it occured, how long it lasted, and the name
-## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-from src.parse_log_line import event_parsing_string as eps
-def get_parsed_data_from_file(logfile, time_range_seconds=None, ignore_crashes = False):
-    assert isinstance(logfile, str)  # input must be a string
-    
-    if not logfile:
-        print("No logfile provided in get_parsed_data_from_file")
-        return
-    table = __manyMatch_LineSearch(eps(), logfile)
-    if not any(table):
-        print("Unable to parse file " + str(logfile))
-        return pd.DataFrame()
-    # Some data collected is read in as a string, but needs to be interpreted as a float. Fix.
-    table[2] = list(map(__number_to_float, table[2]))
-    table[1] = list(map(__number_to_float, table[1]))
-    table[8] = list(map(__number_to_float, table[8]))
-    table[6] = __choose_non_zero(table[6], table[10]) # Before GC collection mem_size
-    table[7] = __choose_non_zero(table[7], table[11]) # After GC collection mem_size
-    table[8] = __choose_non_zero(table[8], table[12]) # Max heapsize
-    table[6] = list(map(__number_to_float, table[6])) 
-    table[7] = list(map(__number_to_float, table[7]))
-    table[8] = list(map(__number_to_float, table[8])) 
-    table[9] = list(map(__number_to_float, table[9])) 
-    # Schema 1 (JDK 16)
-    temp = []
-    table[3] = set_safepoints_eventype(table[3], table[12], table[18])
-    
-    # table [10]  safepoint_name
-    table[12] = list(map(__number_to_float, table[13])) # time_since_last_safepoint
-    table[13] = list(map(__number_to_float, table[14])) # reaching_safepoint_time
-    table[14] = list(map(__number_to_float, table[15])) # at_safepoint_time
-    table[15] = list(map(__number_to_float, table[16])) # total_time_safepoint
-    table[16] = list(map(__number_to_float, table[17])) # total_application_thread_stopped_time_seconds
-    table[17] = list(map(__number_to_float, table[18])) # total_time_to_stop_seconds
-
-    table.pop(12) # Used due to 2 types of memory change regex groups & before/after for each
-    table.pop(11) # Therefore, we gather before & after into 2 distinct columns, and remove other set
-    table.pop(10) 
-
-    parsed_data_table = pd.DataFrame(table).transpose() 
-    # The data collected is in a 2d array, where table indicies represent a column. However,
-    # a pandas dataframe expects each entry of the 2d array to be a row, not a column. Transpose
-    # to fix this orientation error.
-    parsed_data_table.columns = columnNames()  # add column titles, allow for clear references
-    print(parsed_data_table)
-    if time_range_seconds:
-        min_time, max_time = __get_time_range(time_range_seconds)
-        # Get the maximum and minimums and enforce the time range
-        in_minimum = parsed_data_table["TimeFromStart_seconds"] >= min_time
-        in_maximum = parsed_data_table["TimeFromStart_seconds"] <= max_time
-        # Create the combined time table
-        parsed_data_table = parsed_data_table[in_minimum & in_maximum] # Uses true from both other sections
-    if ignore_crashes:
-        return fix_timing_errors(parsed_data_table)
-    else:
-        if check_no_time_errors(parsed_data_table):
-            return parsed_data_table
-        else:
-            print("Warning: Time error noticed in " + logfile+ ". This is typically due to a crash during runtime. Please locate the reset, split the logs into two sections, and run again.")
-            return pd.DataFrame() 
-
-#       check_no_time_errors
-#
-#   Looks through all rows of a gc_event_dataframe timing, and confirms
-#   that there are no timing errors, meaning the time line never decreases.
-#   For all lines x, time(x) <= time(x + n), n > 0. Otherwise, return false.
-#
-def check_no_time_errors(gc_event_dataframe):
-    maximum_time = -1
-    for time in gc_event_dataframe["TimeFromStart_seconds"]:
-        if time < maximum_time:
-            return False
-        else:
-            maximum_time = time
-    return True
-
-#       fix_timing_errors
-#
-#   Given a log file that has a timing error due to a crash,
-#   replace all timing "from start" values after the crash with
-#   the time they WOULD have been had there been no crash.
-#
-def fix_timing_errors(gc_event_dataframe):
-    maximum_time = 0 
-    add_maximum_time = 0
-
-    # Loop through all data
-    for index in range(len(gc_event_dataframe["TimeFromStart_seconds"])):
-        time = gc_event_dataframe["TimeFromStart_seconds"][index]
-        # If we reach a crash reset, keep the maximum time we had before
-        
-        if time + add_maximum_time < maximum_time:
-            add_maximum_time = maximum_time
-        
-
-        # Every row keeps their inital value, added to the shift value from any timing errors
-        gc_event_dataframe["TimeFromStart_seconds"][index] = time + add_maximum_time
-        maximum_time = time + add_maximum_time
-    
-    return gc_event_dataframe
-
-
-
-# Confirms the passed time range, which is either a maximum or a 
-# range. Returns the minimum & maximum times, as a float.
-def __get_time_range(time_range):
-    if type(time_range) == int or type(time_range) == float:
-        min_time = 0
-        max_time = time_range
-    else:
-        #   assert len(time_range) == 2
-        min_time = time_range[0]
-        max_time = time_range[1]
-    return min_time, max_time
-
-#   Confirm that the parameter is numeric
-def __number_to_float(number):
-    if number != None:
-        return float(number)
-    return None
-
-
-## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #                         __manyMatch_LineSearch
 #   Purpose:
 #       Search through a file and return a list of matches for a regex search term
@@ -284,9 +149,25 @@ def set_safepoints_eventype(eventtype_list, safepoint_list1, safepoint_list2):
     return temp
 
 from src.temporary_parse_log_file import event_parsing_string_temp
-def get_parsed_data_from_file_updated(logfile,  time_range_seconds, ignore_crashes = False):
 
-    
+
+## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                               get_parsed_data_from_file
+#
+# Purpose:
+#   Given a filepath to a log file, create a pandas dataframe holding all information
+#   for that log file. Each row is a different event.
+#
+# Parameters:
+#   logfile -> a string with the path to the log file to read.
+#   time_range_seconds -> The time range to parse. Either a range, or just a max.
+#   ignore_crashes -> If true, crashes are ignored, and the program consideres the runtime to be continuous.
+#
+# Return:
+#   a pandas dataframe, where rows are each an individual event. Columns are labeled, and
+#   collect information on each event, such as when it occured, how long it lasted, and the name
+## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+def get_parsed_data_from_file_updated(logfile,  time_range_seconds, ignore_crashes = False):
     regex_capture_string, column_names, data_types = event_parsing_string_temp()    
     table = __manyMatch_LineSearch(regex_capture_string, logfile)
     
@@ -322,7 +203,19 @@ def get_parsed_data_from_file_updated(logfile,  time_range_seconds, ignore_crash
                                 table_groups["SafepointName"], 
                                 table_groups["TimeToStopApplication_seconds"])
     df = pd.DataFrame(table_groups)
-    df.replace({np.nan: None}, inplace= True)
+    
+    ## Clean data, apply resrictions as needed **
+    df.replace({np.nan: None}, inplace= True)    
+    if ignore_crashes:
+        if not assert_no_timing_errors(df):
+            df = fix_timing_errors(df)
+    if time_range_seconds:
+        min_time, max_time = __get_time_range(time_range_seconds)
+        # Get the maximum and minimums and enforce the time range
+        in_minimum = df["TimeFromStart_seconds"] >= min_time
+        in_maximum = df["TimeFromStart_seconds"] <= max_time
+        # Create the combined time table
+        df = df[in_minimum & in_maximum] # Uses true from both other sections
 
     return df
 
@@ -352,3 +245,68 @@ def __create_column(table, data_types, table_columns):
             if not found:
                 new_column.append(None)
         return new_column
+
+#       assert_no_timing_errors
+#
+#   Looks through all rows of a gc_event_dataframe timing, and confirms
+#   that there are no timing errors, meaning the time line never decreases.
+#   For all lines x, time(x) <= time(x + n), n > 0. Otherwise, return false.
+#
+def assert_no_timing_errors(gc_event_dataframe):
+    maximum_time = -1
+    for time in gc_event_dataframe["TimeFromStart_seconds"]:
+        if time < maximum_time:
+            return False
+        else:
+            maximum_time = time
+    return True
+
+#       fix_timing_errors
+#
+#   Given a log file that has a timing error due to a crash,
+#   replace all timing "from start" values after the crash with
+#   the time they WOULD have been had there been no crash.
+#
+def fix_timing_errors(gc_event_dataframe):
+    maximum_time = 0 
+    add_maximum_time = 0
+
+    # Loop through all data
+    for index in range(len(gc_event_dataframe["TimeFromStart_seconds"])):
+        time = gc_event_dataframe["TimeFromStart_seconds"][index]
+        # If we reach a crash reset, keep the maximum time we had before
+        
+        if time + add_maximum_time < maximum_time:
+            add_maximum_time = maximum_time
+
+        # Every row keeps their inital value, added to the shift value from any timing errors
+        gc_event_dataframe["TimeFromStart_seconds"][index] = time + add_maximum_time
+        maximum_time = time + add_maximum_time
+    
+    return gc_event_dataframe
+
+# Confirms the passed time range, which is either a maximum or a 
+# range. Returns the minimum & maximum times, as a float.
+def __get_time_range(time_range):
+    if type(time_range) == int or type(time_range) == float:
+        min_time = 0
+        max_time = time_range
+    else:
+        #   assert len(time_range) == 2
+        min_time = time_range[0]
+        max_time = time_range[1]
+    return min_time, max_time
+
+def zero_start_times(gc_event_dataframes, set_times_to_zero):
+    for index, (df, condition) in enumerate(zip(gc_event_dataframes, set_times_to_zero)):
+        if condition and type(condition) == type(False):
+            if "TimeFromStart_seconds" in df:
+                min_time = df["TimeFromStart_seconds"].iloc[0] 
+                new_timestamps = []
+                for time in df["TimeFromStart_seconds"]:
+                    new_timestamps.append(time - min_time)
+                df["TimeFromStart_seconds"] = new_timestamps
+            else:
+                print("TimeFromStart_seconds not found in gc_event_dataframes index " + str(index))
+    # No return needed, as the originals have been updated.
+            
